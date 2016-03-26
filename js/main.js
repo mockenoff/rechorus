b0975cb135be6797687c6b322dae1d333c03321d
mockenoff <mockenoff@yahoo.com>
2016-03-26 07:38:42 -0500

    Heatmap the density graph

--- js/main.js
+++ js/main.js
@@ -75,11 +75,56 @@ var DATA = [2, 2, 0, 6, 10, 18, 4, 12, 15, 6, 8, 10, 1, 3, 6, 9, 15, 8, 9, 17, 1
 DATA.unshift(0);
 DATA.push(DATA[DATA.length - 1]);
 // Turn them into minute-based tuples
-for (var i = 0, l = DATA.length; i < l; i++) {
+for (var i = 0, dLength = DATA.length; i < dLength; i++) {
 	DATA[i] = [i, DATA[i]];
 }
 // Append SVG
 var svg = d3.select(progress.parentNode).append('svg').attr('width', progress.clientWidth).attr('height', progress.clientHeight);
+// Create gradients
+var gRange = [
+	'rgba(0, 255, 255, 0.5)',
+	'rgba(0, 255, 255, 1)',
+	'rgba(0, 191, 255, 1)',
+	'rgba(0, 127, 255, 1)',
+	'rgba(0, 63, 255, 1)',
+	'rgba(0, 0, 255, 1)',
+	'rgba(0, 0, 223, 1)',
+	'rgba(0, 0, 191, 1)',
+	'rgba(0, 0, 159, 1)',
+	'rgba(0, 0, 127, 1)',
+	'rgba(63, 0, 91, 1)',
+	'rgba(127, 0, 63, 1)',
+	'rgba(191, 0, 31, 1)',
+	'rgba(255, 0, 0, 1)',
+];
+var gLength = gRange.length;
+var dRange = d3.extent(DATA, function(d) { return d[1]; });
+var gradient = svg.append('defs').append('linearGradient')
+	.attr('id', 'gradient')
+	.attr('x1', '0%')
+	.attr('y1', '0%')
+	.attr('x2', '100%')
+	.attr('y2', '0%')
+	.attr('spreadMethod', 'pad');
+for (i = 0; i < dLength; i++) {
+	console.log('A', Math.max(0, Math.floor(gLength * ((DATA[i][1] - dRange[0]) / (dRange[1] - dRange[0]))) - 1));
+	gradient.append('stop')
+		.attr('offset', ((i / dLength) * 100)+'%')
+		.attr('stop-color', gRange[Math.max(0, Math.floor(gLength * ((DATA[i][1] - dRange[0]) / (dRange[1] - dRange[0]))) - 1)])
+		.attr('stop-opacity', 1);
+}
+// gradient.append('stop')
+// 	.attr('offset', '0%')
+// 	.attr('stop-color', 'rgb(255, 138, 0)')
+// 	.attr('stop-opacity', 1);
+// gradient.append('stop')
+// 	.attr('offset', '46%')
+// 	.attr('stop-color', 'rgb(234, 255, 0)')
+// 	.attr('stop-opacity', 1);
+// gradient.append('stop')
+// 	.attr('offset', '98%')
+// 	.attr('stop-color', 'rgb(80, 208, 46)')
+// 	.attr('stop-opacity', 1);
 
 function createGraph() {
 	var x = d3.time.scale().range([0, progress.clientWidth]);
@@ -87,5 +132,5 @@ function createGraph() {
 	x.domain(d3.extent(DATA, function(d) { return d[0]; }));
 	y.domain(d3.extent(DATA, function(d) { return d[1]; }));
 	var area = d3.svg.area().interpolate('basis').x(function(d) { return x(d[0]); }).y0(progress.clientHeight).y1(function(d) { return y(d[1]); });
-	svg.append('path').datum(DATA).attr('class', 'area').attr('d', area);
+	svg.append('path').datum(DATA).style('fill', 'url(#gradient)').attr('d', area);
 }
