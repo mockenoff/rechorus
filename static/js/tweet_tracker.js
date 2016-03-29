a1ba0cf04984f89844b0283bcdd1307478e07ba7
mockenoff <mockenoff@yahoo.com>
2016-03-29 11:49:59 -0500

    Render tweets once they're loaded

--- static/js/tweet_tracker.js
+++ static/js/tweet_tracker.js
@@ -21,6 +21,7 @@ function TweetTracker(container, settings) {
 			onLoadTweets: null,
 		};
 
+
 	// Extend the default settings
 	if (typeof settings !== 'object') {
 		settings = {};
@@ -30,6 +31,34 @@ function TweetTracker(container, settings) {
 			settings[key] = defaultSettings[key];
 		}
 	}
+
+	// Get the templates
+	var marker = /\[\[\s*([\w\d_]+)\s*\]\]/gim,
+		templates = {
+			tweet: 'tweet-template',
+			list: 'media-list-template',
+			item: 'media-item-template',
+		};
+	for (var key in templates) {
+		templates[key] = document.querySelector('#' + templates[key]).innerHTML;
+		templates[key] = templates[key].replace(marker, '[[$1]]');
+	}
+
+
+	// Fill templates with data
+	this.renderTemplate = function(template, data) {
+		if (template in templates === false) {
+			return console.error('No template named ' + template);
+		}
+
+		var render = templates[template];
+		for (var key in data) {
+			render = render.replace(new RegExp('\\[\\['+key+'\\]\\]', 'gim'), data[key]);
+		}
+
+		return render.replace(marker, '');
+	}.bind(this);
+
 
 	// Determine full ranges
 	if (typeof settings.start === 'string') {
@@ -41,7 +70,6 @@ function TweetTracker(container, settings) {
 	if (typeof settings.duration === 'string') {
 		settings.duration = parseInt(settings.duration, 10);
 	}
-	console.log('SETTINGS', settings.start, settings.end, settings.duration);
 	if (typeof settings.duration === 'number') {
 		if (typeof settings.start === 'object') {
 			settings.end = new Date(settings.start.getTime() + (settings.duration * 1000));
@@ -49,8 +77,9 @@ function TweetTracker(container, settings) {
 			settings.start = new Date(settings.end.getTime() - (settings.duration * 1000));
 		}
 	}
-	console.log('SETTINGS', settings.start, settings.end, settings.duration);
 
+
+	// Load tweets based on settings
 	this.loadTweets = function() {
 		if (settings.start === null || settings.end === null) {
 			console.error('Missing settings', settings);
@@ -63,15 +92,38 @@ function TweetTracker(container, settings) {
 			}
 
 			console.log('TWEETS', data);
+			isLoaded = true;
 			tweets = data.tweets;
 			minutes = data.minutes;
+
+			var html = '';
+			for (var i = 0, l = tweets.length; i < l; i++) {
+				var media = '';
+				if (tweets[i].media !== undefined) {
+					for (var j = 0, k = tweets[i].media.length; j < k; j++) {
+						media += this.renderTemplate('item', tweets[i].media[j]);
+					}
+					media = this.renderTemplate('list', {images: media});
+				}
+
+				html += this.renderTemplate('tweet', {
+					media: media,
+					created_at: tweets[i].created_at,
+					screen_name: tweets[i].user.screen_name,
+					formatted_text: tweets[i].formatted_text,
+					profile_image_url: tweets[i].user.profile_image_url,
+				});
+			}
+			container.innerHTML = html;
 
 			if (typeof settings.onLoadTweets === 'function') {
 				settings.onLoadTweets(tweets, minutes);
 			}
-		});
+		}.bind(this));
 	}.bind(this);
 
+
+	// Turn an object of minutes into an array of amplitude
 	this.morphMinutes = function(data) {
 		var morphed = []
 			start = Math.floor(settings.start.getTime() / (60 * 1000));
